720d7739f1f1b447fcc45616a8ac6750d9a398cc
Elaine Anderson <eparie@bu.edu>
2018-07-06 15:56:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    t = Time.now <=> 12
+    t = Time.now <=> 11:59
     if t = -1
       resp.write "Good Morning!"
     elsif t = 1
