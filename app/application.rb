b8c2fe5bb91bc7a7218003dd0b5f6b0cd0451670
Elaine Anderson <eparie@bu.edu>
2018-07-06 16:00:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,10 @@
-require 'pry'
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
 
     t = Time.now <=> 12
-    binding.pry
     if t = -1
       resp.write "Good Morning!"
     elsif t = 0 || t = 1
